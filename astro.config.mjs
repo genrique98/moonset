184060a4afbe9086d1ce4a2e74b6842124c69a5c
gabriel <15620759+genrique98@users.noreply.github.com>
2023-03-08 22:42:21 -0800

    added tailwind typography

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from "astro/config";
 import tailwind from '@astrojs/tailwind';
 import mdx from "@astrojs/mdx";
-
 import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://moonset.work",
   integrations: [mdx(), sitemap(), tailwind()],
 });
