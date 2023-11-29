8b6139451393f8c06125dab8a0637321d58ce158
gabriel <15620759+genrique98@users.noreply.github.com>
2023-11-28 21:58:55 -0800

    update config files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from "astro/config";
+import preact from "@astrojs/preact";
 import tailwind from '@astrojs/tailwind';
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://moonset.work",
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [preact(), mdx(), sitemap(), tailwind()],
 });
