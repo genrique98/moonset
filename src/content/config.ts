c90cd3e24e4c31e4d9bc7c43030a369f0d7e3340
gabriel <15620759+genrique98@users.noreply.github.com>
2024-10-01 13:49:48 -0700

    added content categories

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,21 +1,39 @@
 import { defineCollection, z } from "astro:content";
+// import { CATEGORIES } from '@/data/categories'
+export const CATEGORIES = [
+	"Category 1",
+	"Category 2",
+	"Category 3",
+	"Category 4",
+	"Category 5",
+] as const;
 
 const blog = defineCollection({
-  // Type-check frontmatter using a schema
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    // Transform string to Date object
-    pubDate: z
-      .string()
-      .or(z.date())
-      .transform((val) => new Date(val)),
-    updatedDate: z
-      .string()
-      .optional()
-      .transform((str) => (str ? new Date(str) : undefined)),
-    heroImage: z.string().optional(),
-  }),
+	// Type-check frontmatter using a schema
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		// Transform string to Date object
+		pubDate: z
+			.string()
+			.or(z.date())
+			.transform((val) => new Date(val)),
+		updatedDate: z
+			.string()
+			.optional()
+			.transform((str) => (str ? new Date(str) : undefined)),
+		heroImage: z.string().optional(),
+		category: z.enum(CATEGORIES),
+		tags: z.array(z.string()),
+		draft: z.boolean().default(false),
+	}),
 });
 
 export const collections = { blog };
+
+// const blog = defineCollection({
+// 	schema: ({ image }) =>
+// 		z.object({
+// 			heroImage: image(),
+// 		})
+// })
